7f64120d4468755fdd6c278624fa77b384269382
Siva <sivmani6987@gmail.com>
2021-07-15 12:46:35 +0530

    comit

---
+++ collection/src/collection/Run3.java
@@ -0,0 +1,21 @@
+package collection;
+import java.util.TreeSet;
+
+public class Run3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet ts=new TreeSet();
+		ts.add(new Integer(34));
+		ts.add(new Integer(76));
+		ts.add(new Integer(31));
+		ts.add(new Integer(61));
+		System.out.println(ts);
+	}
+		
+		
+		
+
+	}
+
+
